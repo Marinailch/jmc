2e4aeef344cf55dd091845a0059bf56cc2e2eb04
s7eel <s7eel@me>
2017-03-26 18:35:01 +0300

    2nd

---
+++ Config/config.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Админ
+ * Date: 25.03.2017
+ * Time: 23:51
+ */
---
+++ Library/DataBase.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Админ
+ * Date: 25.03.2017
+ * Time: 23:49
+ */
+namespace Library;
+
+class DataBase
+{
+
+}
